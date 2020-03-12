74e213131add91194c7c166325b76370c9c64373
unknown <vadimkashkin@mail.ru>
2020-03-12 18:06:03 +0300

    мучаю усложнённое 20-ое

--- js/main.js
+++ js/main.js
@@ -2,14 +2,25 @@ document.addEventListener("DOMContentLoaded", function (event) {
     const modal = document.querySelector('.modal');
     const modalBtn = document.querySelectorAll('[data-toggle=modal]');
     const closeBtn = document.querySelector('.modal__close');
+    const formBlock = document.querySelector('.modal__dialog');
     const switchModal = () => {
         modal.classList.toggle('modal--visible');
-    }
+    };
     modalBtn.forEach(element => {
         element.addEventListener('click', switchModal);
     });
-/*     document.addEventListener('keypress', function (e) {
-        if(e.keyCode === 27) document.getElementById('modal_id').hidden= 1;
-    });  */
     closeBtn.addEventListener('click', switchModal);
+    modal.addEventListener('click', switchModal);
+});
+/* Клик по фону */
+/*     modal.addEventListener('click', switchModal); */
+
+
+/* Нажатие кнопки */
+document.addEventListener('keypress', function (event) {
+    if (e.keyCode === 27) {
+        (switchModal);
+    } else {
+        console.log('Хрен угадал, попробуй ещё');
+    }
 });
