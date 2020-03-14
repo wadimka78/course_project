28128738490a540c2dee42d5d2e57a23e5cf7172
unknown <vadimkashkin@mail.ru>
2020-03-14 10:22:59 +0300

    feat: close modal by click outside and key Escape

--- js/main.js
+++ js/main.js
@@ -2,25 +2,32 @@ document.addEventListener("DOMContentLoaded", function (event) {
     const modal = document.querySelector('.modal');
     const modalBtn = document.querySelectorAll('[data-toggle=modal]');
     const closeBtn = document.querySelector('.modal__close');
-    const formBlock = document.querySelector('.modal__dialog');
     const switchModal = () => {
         modal.classList.toggle('modal--visible');
     };
     modalBtn.forEach(element => {
         element.addEventListener('click', switchModal);
+        closeBtn.addEventListener('click', switchModal);
     });
-    closeBtn.addEventListener('click', switchModal);
-    modal.addEventListener('click', switchModal);
-});
-/* Клик по фону */
-/*     modal.addEventListener('click', switchModal); */
 
 
-/* Нажатие кнопки */
-document.addEventListener('keypress', function (event) {
-    if (e.keyCode === 27) {
-        (switchModal);
-    } else {
-        console.log('Хрен угадал, попробуй ещё');
-    }
+    /* Клик по фону */
+
+    modal.addEventListener("click", function (event) {
+        var target = event.target;
+        if (target.closest(".modal__dialog") && !target.closest(".form__input") && !target.closest(".form__label"))
+            event.stopPropagation();
+        else
+            modal.classList.toggle('modal--visible');
+    });
+
+    /* Клик по кнопке */
+
+    document.addEventListener("keydown", function (event) {
+        if (event.key === "Escape") {
+            modal.classList.toggle('modal--visible');
+        } /* else {
+            alert('А вот хрен угадал. Пробуй ещё ))');
+        } */
+    });
 });
