ef81d56f3c2cd6e4ab0801116e67480269d54e76
unknown <vadimkashkin@mail.ru>
2020-03-14 13:47:26 +0300

    feat: rewrite on jQwery and add the button-scroll

---
+++ js/qwery.js
@@ -0,0 +1,76 @@
+/* document.addEventListener("DOMContentLoaded", function (event) {
+    const modal = document.querySelector('.modal');
+    const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+    const closeBtn = document.querySelector('.modal__close');
+    const switchModal = () => {
+        modal.classList.toggle('modal--visible');
+    };
+    modalBtn.forEach(element => {
+        element.addEventListener('click', switchModal);
+        closeBtn.addEventListener('click', switchModal);
+    });
+
+
+
+
+    modal.addEventListener("click", function (event) {
+        var target = event.target;
+        if (target.closest(".modal__dialog") && !target.closest(".form__input") && !target.closest(".form__label"))
+            event.stopPropagation();
+        else
+            modal.classList.toggle('modal--visible');
+    });
+
+
+    document.addEventListener("keydown", function (event) {
+        if (event.key === "Escape") {
+            modal.classList.toggle('modal--visible');
+        } 
+    });
+});
+ */
+
+ $(document).ready(function() {
+   var modal = $('.modal'),
+   modalBtn = $('[data-toggle=modal]');
+   closeBtn = $('.modal__close');
+
+    modalBtn.on('click', function() {
+        modal.toggleClass ('modal--visible');
+    });
+    closeBtn.on('click', function () {
+        modal.toggleClass ('modal--visible');
+    });
+
+    /*Закрытие по фону*/
+
+    $(document).click(function (e) {
+        if ($(e.target).is('.modal')) {
+            modal.toggleClass ('modal--visible');
+        }
+    });    
+
+    /*Закрытие по кнопке*/
+
+    $(document).keydown(function (e) {
+        if (e.keyCode === 27) {
+            modal.toggleClass ('modal--visible');
+        }
+    });   
+
+    /*Кнопка прокрутки*/
+	$(window).scroll(function(){
+		if($(window).scrollTop() > 100) {
+			$('#scroll_top').show();
+		} else {
+			$('#scroll_top').hide();
+		}
+	});
+
+	$('#scroll_top').click(function(){
+		$('html, body').animate({scrollTop: 0}, 600);
+		return false;
+	});
+
+});
+
