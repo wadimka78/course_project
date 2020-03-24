097fc9ad546f12e8639a68bab71d6f0ae5c182c2
unknown <vadimkashkin@mail.ru>
2020-03-24 16:45:23 +0300

    почти закончил адаптацию на 375 экране

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
-const {src, dest, watch} = require('gulp'),
+
+const {src, dest, watch, series} = require('gulp'),
     browserSync = require('browser-sync').create(),
     cleanCSS = require('gulp-clean-css'),
+   // minify = require('gulp-minify'),
     rename = require('gulp-rename'),
     sass = require('gulp-sass');
     autoprefixer = require('gulp-autoprefixer');
-
 // Static server
- function bs() {
-     serveSass();
+function bs() {
+    serveSass();
     browserSync.init({
         server: {
             baseDir: "./"
@@ -18,7 +19,6 @@ const {src, dest, watch} = require('gulp'),
     watch("./scss/**/*.scss", serveSass);
     watch("./js/*.js").on('change', browserSync.reload);
 };
-
 // Compile sass into CSS & auto-inject into browsers
 function  serveSass() {
     return src("./sass/**/*.sass", "./scss/**/*.scss")
@@ -29,13 +29,25 @@ function  serveSass() {
         .pipe(dest("./css"))
         .pipe(browserSync.stream());
 };
+function  buildCSS(done) {
+    src('./css/**/**.css', './css/**/**.min.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(dest('dist/css'))
+    done();
+}
+//function  buildJs(done) {
+  //  src(['./js/**/**.js', './js/**/**.min.js'])
+    //.pipe(minify())
+   // .pipe(dest('dist/js'))
+    //done();
+//}
 
-exports.serve = bs; 
+exports.serve = bs;
+exports.build = series(buildCSS);
 
 // Минимизация CSS и добавление к ним .min
 
 /* gulp.task('mincss', function() {
-
     return src("./css/mystyle.css")
     .pipe(rename({suffix: ".min"}))
     .pipe(cleanCSS())
